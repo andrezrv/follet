37dee2a2d1cdff9a9d5462fb23ea67542f9ddd78
andrezrv <andrezrv@gmail.com>
2014-04-05 09:49:41 -0300

    Improved support for Jetpack styles.

--- js/bootstrap-support.js
+++ js/bootstrap-support.js
@@ -8,6 +8,7 @@ jQuery( document ).ready( function( $ ) {
 	$( 'table' ).addClass( 'table' );
 	$( 'input[type="button"], input[type="submit"], button' ).addClass( 'btn' );
 	$( '.pushbutton-wide' ).addClass( 'btn-primary' ).addClass( 'btn-lg' );
+	$( '#subscribe-submit .btn' ).addClass( 'btn-primary' ).addClass( 'btn-sm' );
 	$( 'input[type="text"], input[type="password"], input[type="email"], select, textarea' ).addClass( 'form-control' );
 	$( '#primary select, #secondary select, #footer select' ).addClass( 'form-control' );
 });

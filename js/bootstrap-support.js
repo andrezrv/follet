00e229dcfa5425a63af52dc3e10f33a353ac3510
andrezrv <andrezrv@gmail.com>
2014-05-12 23:47:08 -0300

    Close #24.

--- js/bootstrap-support.js
+++ js/bootstrap-support.js
@@ -7,7 +7,7 @@
 jQuery( document ).ready( function( $ ) {
 
 	$( 'table' ).addClass( 'table' );
-	$( 'input[type="button"], input[type="submit"], button' ).addClass( 'btn' );
+	$( 'input[type="button"], input[type="submit"], input[type="reset"], button' ).addClass( 'btn').addClass( 'btn-secondary' );
 	$( '.pushbutton-wide' ).addClass( 'btn-primary' ).addClass( 'btn-lg' );
 	$( '#subscribe-submit .btn' ).addClass( 'btn-primary' ).addClass( 'btn-sm' );
 	$( 'input[type="text"], input[type="password"], input[type="email"], select, textarea' ).addClass( 'form-control' );
